33bbea77b60565f06a817c2c594d8ac3e38700b4
Matthew Purland <m.purland@gmail.com>
2015-11-18 19:47:11 -0800

    Refactored JSONDecoder to use lift either pattern.

--- Tyro/Decoder.swift
+++ Tyro/Decoder.swift
@@ -16,41 +16,10 @@ public protocol JSONDecoder {
 extension JSONDecoder {
     public static func decode(value: AnyObject) -> Either<JSONError, JSONValue> {
         switch value {
-        case let value as [AnyObject]:
-            let errorValuePairs: [(JSONError?, JSONValue?)] = value.flatMap(decode).map({ (either) -> (JSONError?, JSONValue?) in
-                return either.either(onLeft: { (error) -> (JSONError?, JSONValue?) in
-                    return (error, nil)
-                    }, onRight: { (value) -> (JSONError?, JSONValue?) in
-                        return (nil, value)
-                })
-            })
-            
-            let errors: [JSONError] = errorValuePairs.flatMap { $0.0 }
-            
-            if errors.count > 0 {
-                return .Left(.Array(errors))
-            }
-            else {
-                return .Right(.Array(errorValuePairs.flatMap { $0.1 }))
-            }
+        case let values as [AnyObject]:
+            return values.flatMap(decode).lift().either(onLeft: { .Left(.Array($0)) }, onRight: { .Right(.Array($0)) })
         case let value as [Swift.String: AnyObject]:
-            let errorValuePairs: [(JSONError?, (Swift.String, JSONValue)?)] = value.flatMap(decode).map({ (either) -> (JSONError?, (Swift.String, JSONValue)?) in
-                return either.1.either(onLeft: { (error) -> (JSONError?, (Swift.String, JSONValue)?) in
-                    return (error, nil)
-                    }, onRight: { (value) -> (JSONError?, (Swift.String, JSONValue)?) in
-                        return (nil, (either.0, value))
-                })
-            })
-            
-            let errors: [JSONError] = errorValuePairs.flatMap { $0.0 }
-            
-            if errors.count > 0 {
-                return .Left(.Array(errors))
-            }
-            else {
-                let values: [Swift.String: JSONValue] = Dictionary( errorValuePairs.flatMap { $0.1 })
-                return .Right(.Object(values))
-            }
+            return value.flatMap(decode).lift().either(onLeft: { .Left(.Array($0)) }, onRight: { .Right(.Object($0)) })
         case let value as Swift.String:
             return .Right(.String(value))
         case let value as NSNumber:
